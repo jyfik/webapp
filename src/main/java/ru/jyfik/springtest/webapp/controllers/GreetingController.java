f48ac0566986d4415c25591fa0395c7a27c9be68
jyfik <vlasov228daniil@gmail.com>
2021-03-15 15:08:33 +0300

    Стилевые изменения.

--- src/main/java/ru/jyfik/springtest/webapp/controllers/GreetingController.java
+++ src/main/java/ru/jyfik/springtest/webapp/controllers/GreetingController.java
@@ -3,16 +3,16 @@ package ru.jyfik.springtest.webapp.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Map;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class GreetingController {
 
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Map<String, String> model) {
-        model.put("name", name);
-        return "hello";
+    public ModelAndView greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, ModelAndView model) {
+        model.addObject("name", name);
+        model.setViewName("hello");
+        return model;
     }
 
     @GetMapping
